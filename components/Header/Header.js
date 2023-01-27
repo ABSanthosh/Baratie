81b71868d59efbb7289367030a49291062ba7413
ABSanthosh <a.b.santhosh02@gmail.com>
2023-01-27 20:57:42 +0530

    feat: landing page

---
+++ components/Header/Header.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Header.scss";
+import Logo from "../Logo/Logo";
+import Link from "next/link";
+import useAuth from "../../hooks/useAuth";
+import { useRouter } from "next/router";
+
+function Header({ currentItem, user }) {
+  const router = useRouter();
+  const { login } = useAuth();
+  return (
+    <div className="HeaderWrapper">
+      <div className="Header">
+        <Logo />
+        <ul className="Header__menu">
+          <li
+            className={`Header__menu--${
+              currentItem === "home" ? "activeItem" : "item"
+            }`}
+          >
+            <Link href="/">Home</Link>
+          </li>
+          <li
+            className={`Header__menu--${
+              currentItem === "categories" ? "activeItem" : "item"
+            }`}
+          >
+            <Link href="/categories">Categories</Link>
+          </li>
+          <li
+            className={`Header__menu--${
+              currentItem === "about" ? "activeItem" : "item"
+            }`}
+          >
+            <Link href="/about">About</Link>
+          </li>
+          <li
+            className={`Header__menu--${
+              currentItem === "contact" ? "activeItem" : "item"
+            }`}
+          >
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+        <div className="Header__actions">
+          {user === null ? (
+            <button
+              className="Header__actions--button dark"
+              data-icon={String.fromCharCode(60023)}
+              title="Sign in with Google"
+              onClick={async () => await login(router)}
+            />
+          ) : (
+            <Link href="/app/dashboard">
+              <a
+                className="Header__actions--button dark"
+                data-icon={String.fromCharCode(59505)}
+                title="Cart"
+              />
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import "../styles/routes/Home.scss";
 import { useRouter } from "next/router";
 
 import useAuth from "../hooks/useAuth";
+import Header from "../components/Header/Header";
 
 export async function getServerSideProps(context) {
   if (context.req.session.user === undefined) {
@@ -21,12 +22,30 @@ export default function Home({ user }) {
   const router = useRouter();
   const { login } = useAuth();
 
+  // {user !== null && <a href="/app/dashboard">Open App →</a>}
+
   return (
     <div className="HomeWrapper">
-      {user === null && (
-        <button onClick={async () => await login(router)}>Google Login</button>
-      )}
-      {user !== null && <a href="/app/dashboard">Open App →</a>}
+      <Header currentItem="home" user={user} />
+      <section className="HomeWrapper__hero"></section>
+      <section className="HomeWrapper__promises">
+        <div className="HomeWrapper__promises--item">
+          <img src="/Img/Icons/quality.png" alt="quality" />
+          <p>Best Quality</p>
+        </div>
+        <div className="HomeWrapper__promises--item">
+          <img src="/Img/Icons/money.png" alt="price" />
+          <p>Best Price</p>
+        </div>
+        <div className="HomeWrapper__promises--item">
+          <img src="/Img/Icons/openBox.png" alt="shipment" />
+          <p>Fast Delivery</p>
+        </div>
+        <div className="HomeWrapper__promises--item">
+          <img src="/Img/Icons/support.png" alt="support" />
+          <p>24/7 support</p>
+        </div>
+      </section>
     </div>
   );
 }
